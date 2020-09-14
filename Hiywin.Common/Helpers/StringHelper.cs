71ddf81f98de9319fac1f2731327a6e5b2d9876a
hiywin <hellowin1853@gmail.com>
2020-09-14 10:25:26 +0800

    Sql拼接，采用匿名参数查询，防注入漏洞

--- Hiywin.Common/Helpers/StringHelper.cs
+++ Hiywin.Common/Helpers/StringHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,30 +7,60 @@ namespace Hiywin.Common.Helpers
 {
     public class StringHelper
     {
-        public static void StringAdd(StringBuilder builder, string formater, object param, string condition=" and ")
+        public static void StringAdd(StringBuilder builder, string formater, object paramValue, string condition = " and ")
         {
-            switch (param?.GetType()?.Name)
+            switch (paramValue?.GetType()?.Name)
             {
                 case "String":
-                    if (!string.IsNullOrEmpty(param.ToString()))
+                    if (!string.IsNullOrEmpty(paramValue.ToString()))
                     {
-                        if (builder.Length > 0) builder.Append(condition);
-                        builder.Append(string.Format(formater, param));
+                        if (builder.Length > 0) builder.AppendLine(condition);
+                        builder.AppendLine(string.Format(formater, paramValue));
                     }
                     break;
                 case "Int32":
-                    if ((int)param > 0)
+                    if ((int)paramValue > 0)
                     {
-                        if (builder.Length > 0) builder.Append(condition);
-                        builder.Append(string.Format(formater, param));
+                        if (builder.Length > 0) builder.AppendLine(condition);
+                        builder.AppendLine(string.Format(formater, paramValue));
                     }
                     break;
                 case "Boolean":
-                    if (builder.Length > 0) builder.Append(condition);
-                    builder.Append(string.Format(formater, param));
+                    if (builder.Length > 0) builder.AppendLine(condition);
+                    builder.AppendLine(string.Format(formater, paramValue));
                     break;
                 default:
                     break;
+            }
+        }
+
+        public static void ParameterAdd(StringBuilder builder, string formater, object paramValue, string condition = " and ")
+        {
+            if (!string.IsNullOrEmpty(formater))
+            {
+                switch (paramValue?.GetType()?.Name)
+                {
+                    case "String":
+                        if (!string.IsNullOrEmpty(paramValue.ToString()))
+                        {
+                            if (builder.Length > 0) builder.AppendLine(condition);
+                            builder.AppendLine(formater);
+                        }
+                        break;
+                    case "Int32":
+                        if ((int)paramValue > 0)
+                        {
+                            if (builder.Length > 0) builder.AppendLine(condition);
+                            builder.AppendLine(formater);
+                        }
+                        break;
+                    case "Boolean":
+                        if (builder.Length > 0) builder.AppendLine(condition);
+                        builder.AppendLine(formater);
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
--- Hiywin.FrameService/ModuleService.cs
+++ Hiywin.FrameService/ModuleService.cs
@@ -1,4 +1,5 @@
-﻿using Hiywin.Common;
+﻿using Dapper;
+using Hiywin.Common;
 using Hiywin.Common.Data;
 using Hiywin.Common.Helpers;
 using Hiywin.Entities.Frame;
@@ -20,17 +21,33 @@ namespace Hiywin.FrameService
         {
             var lr = new DataResult<List<ISysModuleModel>>();
 
-            StringBuilder builder=new StringBuilder();
+            #region SQL拼接写法1 - 参数、值直接拼接
+            //StringBuilder builder = new StringBuilder();
+            //string sqlCondition = string.Empty;
+
+            //StringHelper.StringAdd(builder, " ModuleNo = '{0}' ", query.Criteria.ModuleNo);
+            //StringHelper.StringAdd(builder, " ModuleName = '{0}' ", query.Criteria.ModuleName);
+            //StringHelper.StringAdd(builder, " IsDelete = {0} ", query.Criteria.IsDelete);
+
+            //if (builder.Length > 0)
+            //{
+            //    sqlCondition = " where " + builder.ToString();
+            //}
+            #endregion
+            #region SQL拼接写法2 - 使用匿名参数，防注入漏洞
+            StringBuilder builder = new StringBuilder();
             string sqlCondition = string.Empty;
 
-            StringHelper.StringAdd(builder, " ModuleNo = '{0}' ", query.Criteria.ModuleNo);
-            StringHelper.StringAdd(builder, " ModuleName = '{0}' ", query.Criteria.ModuleName);
-            StringHelper.StringAdd(builder, " IsDelete = {0} ", query.Criteria.IsDelete);
+            StringHelper.ParameterAdd(builder, "ModuleNo = @ModuleNo", query.Criteria.ModuleNo);
+            StringHelper.ParameterAdd(builder, "ModuleName = @ModuleName", query.Criteria.ModuleName);
+            StringHelper.ParameterAdd(builder, "IsDelete = @IsDelete", query.Criteria.IsDelete);
 
             if (builder.Length > 0)
             {
                 sqlCondition = " where " + builder.ToString();
             }
+            #endregion
+
             string sql = "select Id,ModuleNo,ModuleName,ParentNo,Icon,Url,Category,Target,IsResource,App,Creator,CreateName,CreateTime,Updator,UpdateName,UpdateTime,IsDelete,Sort " +
                 "from sys_module"
                 + sqlCondition;
@@ -38,7 +55,7 @@ namespace Hiywin.FrameService
             {
                 try
                 {
-                    var modelList = await MysqlHelper.QueryListAsync<SysModuleModel>(dbConn, sql, "Sort asc");
+                    var modelList = await MysqlHelper.QueryListAsync<SysModuleModel>(dbConn, sql, "Sort asc", query.Criteria);
                     lr.Data = modelList.ToList<ISysModuleModel>();
                 }
                 catch (Exception ex)
